782d56671a6b470c8f43d7a13725b05975703b1b
Ethan <ethanhilljay@outlook.com>
2021-05-18 11:16:36 +0100

    Inital Express backend files

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const logger = require("morgan")
+const cors = require("cors")
+
+const PORT = process.env.PORT || 3000
+
+const indexRouter = require("./routes/index")
+
+const app = express()
+
+app.use(cors())
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use("/api", indexRouter)
+
+app.listen(PORT, () => {
+  console.clear()
+  console.log(`Listening on port ${PORT}`)
+})
