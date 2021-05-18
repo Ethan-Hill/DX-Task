48a087e6b5ae7e9e44e044fa1f2daf3367f663c8
Ethan <ethanhilljay@outlook.com>
2021-05-18 11:36:40 +0100

    Updated Port

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const logger = require("morgan")
 const cors = require("cors")
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 const indexRouter = require("./routes/index")
 
